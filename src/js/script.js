8ba4edd1a1a0f87d175efff47fc41c575da3659f
user <user@gmail.com>
2020-03-28 15:41:50 +0300

    Prengi_release

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+//slider
 $(document).ready(function(){
     $('.promo__carousel').slick({
         dots: true,
@@ -16,4 +17,49 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right_arrow_big.svg"></button>',
         speed: 1200
     });
+});
+//modal
+$('[data-modal=communictaion]').on('click', function() {
+    $('.overlay, #communictaion').fadeIn('slow');
+});
+
+$('.modal__close').on('click', function() {
+    $('.overlay, #communictaion').fadeOut('slow');
+});
+//validate and mask
+function valideForms(form){
+    $(form).validate({
+        rules: {
+            name: "required",
+            phone: "required",
+            email: {
+                required: true,
+                email: true
+            },
+        },
+        messages: {
+            name: "Введите имя",
+            phone: "Введите номер телефона",
+            email: {
+              required: "Введите почту",
+              email: "Неправильно введен адрес почты"
+            }
+        }
+    });
+};
+valideForms('#communictaion form');
+$('input[name=phone]').mask("+7 (999) 999-99-99");
+//mailer
+$('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        $('.overlay, #communictaion').fadeOut();
+        $('form').trigger('reset');
+    });
+    return false;
 });
